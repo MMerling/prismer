bcd66b653b0f31403bc661b06948edd136361eaf
Mark Merling <mark.merling@gmail.com>
2023-04-30 12:29:30 -0400

    updating graph images

--- graph_best.py
+++ graph_best.py
@@ -8,14 +8,20 @@ train_prismerz_df = pd.read_json(open('logging/classification_prismerz_base_save
 valid_prismer_df = pd.read_json(open('logging/classification_prismer_base_bakcup__5e-9/epoch_valid.jsonl'), lines=True)
 valid_prismerz_df = pd.read_json(open('logging/classification_prismerz_base_saved_5e-9/epoch_valid.jsonl'), lines=True)
 
-def plot_graph(df1, df2, ylabel, max_epoch, train=False):
+def plot_graph(df1, df2, ylabel, max_epoch, train=False, df3=None, df4=None):
     if train:
-        graph_type = "train"
+        graph_type = "Train"
     else:
-        graph_type = "valid"
+        graph_type = "Valid"
     plt.figure()
-    plt.plot(df1[df1["epoch"]<=max_epoch][ylabel.lower()], label="Prismer Base")
-    plt.plot(df2[ylabel.lower()], label="Prismer ZBase")
+    if ylabel.lower()=="loss":
+        plt.plot(df1[df1["epoch"]<=max_epoch][ylabel.lower()], label="Train Prismer Base")
+        plt.plot(df2[ylabel.lower()], label="Train Prismer ZBase")
+        plt.plot(df3[df1["epoch"]<=max_epoch][ylabel.lower()], label="Valid Prismer Base")
+        plt.plot(df4[ylabel.lower()], label="Valid Prismer ZBase")
+    else:
+        plt.plot(df1[df1["epoch"]<=max_epoch][ylabel.lower()], label=f"{graph_type} Prismer Base")
+        plt.plot(df2[ylabel.lower()], label=f"{graph_type} Prismer ZBase")
     plt.grid(True)
     plt.legend()
     ax = plt.gca()
@@ -30,7 +36,7 @@ def plot_graph(df1, df2, ylabel, max_epoch, train=False):
     plt.savefig(f"graphs/prismer_{ylabel.lower()}_vs_epoch_{graph_type}.png")
     plt.close()
 
-plot_graph(train_prismer_df, train_prismerz_df, "Loss", 11)
-plot_graph(valid_prismer_df, valid_prismerz_df, "Loss", 11)
+plot_graph(train_prismer_df, train_prismerz_df, "Loss", 11, True, valid_prismer_df, valid_prismerz_df)
+# plot_graph(valid_prismer_df, valid_prismerz_df, "Loss", 11)
 plot_graph(valid_prismer_df, valid_prismerz_df, "Acc", 11)
 plot_graph(valid_prismer_df, valid_prismerz_df, "AUROC", 11)
